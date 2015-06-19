cd3352e309609a5aaa71c2f49906e654f7e593a1
aldegtyarev <aldegtyarev@yandex.ru>
2015-06-19 18:20:46 +0300

    1906

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -6,6 +6,7 @@ use yii\bootstrap\Tabs;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use frontend\widgets\Alert;
+use yii\helpers\Url;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -25,7 +26,97 @@ AppAsset::register($this);
 <body>
     <?php $this->beginBody() ?>
     <div class="wrap">
+		<div class="header">
+			<div class="header__row1">
+				<div class="container clearfix">
+
+					<div class="show_specialists clearfix">
+						<span class="show_specialists__ttl">Отображать профессионалов:</span>
+						<div class="header__regions">
+							<div id="header_regions__active" class="header_regions__active">Вся Беларусь</div>
+							<div id="header_regions__list_cnt" class="header_regions__list_cnt">
+								<ul class="header_regions__list">
+								<li class="header_regions__item"><a href="#" data-region="1">Минская область</a></li>
+								<li class="header_regions__item"><a href="#" data-region="2">Гомельская область</a></li>
+								<li class="header_regions__item"><a href="#" data-region="3">Брестская область</a></li>
+								<li class="header_regions__item"><a href="#" data-region="4">Гродненская область</a></li>
+								<li class="header_regions__item"><a href="#" data-region="5">Витебская область</a></li>
+								<li class="header_regions__item"><a href="#" data-region="6">Могилёвская область</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="currency_select">
+						<div class="currency_select__ttl">Выбор валюты:</div>
+						<div class="currency_select__list_cnt">
+							<ul class="currency_select__list">
+								<li class="currency_select__item currency_select__byr_active"><a href="#">BYR</a></li>
+								<li class="currency_select__item currency_select__usd_active_"><a href="#">USD</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="autorization_h">
+						<ul class="autorization_h__list">
+							<li class="autorization_h__item autorization_h__item_reg_spec"><a class="autorization_h__reg_spec" href="#">Стать специалистом</a></li>
+							<li class="autorization_h__item autorization_h__item_login"><a class="autorization_h__login" href="#">Войти</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="header__row2">
+				<div class="container">
+					<div class="row clearfix">
+						<div class="col-lg-5">
+							<a href="#" class="logo_top">
+								<img class="logo_top__img" src="<?php echo Url::to('@web'); ?>/images/logo-top.png" alt="Профессионалы">
+								<span class="logo_top__cnt">
+									<span class="logo_top__sitename">Профессионалы</span>
+									<span class="logo_top__slogan">Только проверенные специалисты</span>
+								</span>
+							</a>
+						</div>
+
+
+						<div class="col-lg-5 col-lg-offset-2">
+							<div class="header_phone">
+								<div class="header_phone__txt"><span class="bold">Единый номер для всех</span><br>мобильных операторов РБ:</div>
+								<div class="header_phone__number"><span class="header_phone__number_cnt">345-89-98</span></div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="header__row3">
+				<div class="container">
+					<span class="header_popular__ttl">Популярные категории</span>
+
+
+					<?php 
+					echo Nav::widget([
+					'options' => [
+					'class' => 'navbar-nav navbar-left header_navbar'
+					],
+					'items' => [
+					'<li><a href="#" class="header_navbar__item"><span>Репетиторы</span>учителя и лекторы</a></li>',
+					'<li><a href="#" class="header_navbar__item"><span>Артисты</span>певцы и ведущие</a></li>',
+					'<li><a href="#" class="header_navbar__item"><span>Мастера красоты</span>учителя и ученики</a></li>',
+					'<li><a href="#" class="header_navbar__item"><span>Фотографы</span>Свадебные съемки</a></li>',
+					'<li><a href="#" class="header_navbar__item"><span>Спортивные тренеры</span>индивидуальные тренера</a></li>',
+					]
+					]);				   
+					?>
+				</div>
+			</div>
+		</div>
+       
         <?php
+		//echo'<pre>';print_r($this);echo'</pre>';
+/*
             NavBar::begin([
                 'brandLabel' => 'My Company',
                 'brandUrl' => Yii::$app->homeUrl,
@@ -53,6 +144,7 @@ AppAsset::register($this);
                 'items' => $menuItems,
             ]);
             NavBar::end();
+			*/
         ?>
 
         <div class="container">
@@ -60,55 +152,33 @@ AppAsset::register($this);
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
+		<div class="profi_search">
+			<p class="profi_search__ttl">Бесплатный подбор профессионалов по вашим критериям</p>
+			
+			<div class="profi_search__tabs">
+				<?php 
+				echo Tabs::widget([
+					'items' => [
+						[
+							'label' => 'Yii2',
+							'content' => $this->render('_tab1', [], false, true),
+							'active' => true
+						],
+						[
+							'label' => 'jQuery',
+							'content' => '<h2>jQuery - один из самых популярных JavaScript фреймворков, который работает с объектами DOM.</h2>'
+						],
+					]
+				]);			
+
+				?>        
+				
+			</div>
+		</div>
+       
+        
         <?= $content ?>
         
-<?php 
-echo Tabs::widget([
-    'items' => [
-        [
-            'label' => 'Yii2',
-            'content' => '<h2>Фреймворк Yii 2 - один из самых быстрых, безопасных и "крутых" php-фреймворков.</h2>',
-            'active' => true
-        ],
-        [
-            'label' => 'jQuery',
-            'content' => '<h2>jQuery - один из самых популярных JavaScript фреймворков, который работает с объектами DOM.</h2>'
-        ],
-        [
-            'label' => 'Bootstrap',
-            'content' => '<h2>Twitter Bootstrap - супер фреймворк, объединяющий в себе html, css, и JavaScript для для верстки веб-интерфейсов и страниц.</h2>',
-            'headerOptions' => [
-                'id' => 'headerOptions'
-            ],
-            'options' => [
-                'id' => 'options'
-            ]
-        ],
-        [
-            'label' => 'Еще табы',
-            'content' => '<h2>Вы можете добавить любое количество табов. Просто опишите их структуру в массиве.</h2>'
-        ],
-        [
-            'label' => 'Выпадающий список табов',
-            'items' => [
-                [
-                    'label' => 'Первый таб из выпадающего списка',
-                    'content' => '<h2>Обновите свои познания в Yii 2 and Twitter Bootstrap. Все возможнсти уже обернуты в удобные интерфейсы.</h2>'
-                ],
-                [
-                    'label' => 'Второй таб из выпадающего списка',
-                    'content' => '<h2>Один в поле не воин, а двое - уже компания.</h2>'
-                ],
-                [
-                    'label' => 'Это третий таб из выпадающего списка',
-                    'content' => '<h2>Третий не лишний!</h2>'
-                ]
-            ]
-        ]
-    ]
-]);			
-			
-?>        
         </div>
     </div>
 
